5a935a04740cd7e589daf48a0496411b51d3ce2f
Muhammad Al Farizzi <muhammadalfarizzi563@gmail.com>
2020-12-04 08:00:55 +0700

    Fix Register System Bug

--- app/Http/Controllers/Auth/AuthRepository.php
+++ app/Http/Controllers/Auth/AuthRepository.php
@@ -10,6 +10,7 @@ use Illuminate\Support\Facades\Auth;
 class AuthRepository extends Controller
 {
     public function postRegister($request) {
+        $request['password'] = bcrypt($request['password']);
         $user = User::create($request);
     }
 
